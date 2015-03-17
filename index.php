835b33998e97a88993637a258052cc0c057ae5b3
Axel Lanuza <axlwolf@gmail.com>
2015-03-17 14:52:30 -0600

    Commit inicial
    Mockup Proyecto Hype!

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    
+</head>
+<body>
+<header>
+    <figure class="logo">
+        <img src="" alt=""/>
+    </figure>
+</header>
+</body>
+</html>
