32d2016e5bb65b6f6abc0eaf6c4e56bc9c7582f4
Axel Lanuza <axlwolf@gmail.com>
2015-03-17 19:09:28 -0600

    Diseño preliminar
    Primer esbozo de la plataforma Hype!

--- index.php
+++ index.php
@@ -3,13 +3,117 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="css/estilo.css"/>
+    <link rel="stylesheet" href="css/font-awesome.css"/>
+
 </head>
 <body>
-<header>
+<header id="principal">
     <figure class="logo">
-        <img src="" alt=""/>
+        <img src="img/logo.jpg" alt="Logo Hype!"/>
     </figure>
+
+    <div class="social">
+        <i class="fa fa-youtube fa-1x"></i>
+        <i class="fa fa-instagram fa-1x"></i>
+        <i class="fa fa-twitter fa-1x"></i>
+        <i class="fa fa-facebook fa-1x"></i>
+    </div>
+
+    <div class="busqueda">
+        <form class="form-inline" action="">
+            <div class="form-group">
+                <input type="text" placeholder="Buscar..."/>
+            </div>
+            <button type="submit" class="btn btn-default"><i class="fa fa-search fa-3x"></i></button>
+        </form>
+    </div>
+
+    <div class="music_widget">
+        <i class="fa fa-volume-up fa-1"></i><br />
+        <i class="fa fa-backward fa-1"></i>
+        <i class="fa fa-play fa-1"></i>
+        <i class="fa fa-forward fa-1"></i>
+    </div>
 </header>
+
+<nav>
+    <ul>
+        <li><a href="#">Noticias</a></li>
+        <li><a href="#">Reseñas</a></li>
+        <li><a href="#">Festivales</a></li>
+        <li><a href="#">Cine</a></li>
+        <li><a href="#">Geek</a></li>
+        <li><a href="#">Descargas</a></li>
+        <li><a href="#">Sesiones</a></li>
+        <li id="agendita"><a href="#"> <i class="fa fa-calendar fa-1x"></i> Agenda</a></li>
+    </ul>
+</nav>
+
+<section class="banner">
+    <figure class="publicidad">
+        <img src="http://lorempixel.com/960/100" alt=""/>
+    </figure>
+</section>
+
+<section id="carrusel">
+    <figure>
+    <img src="http://lorempixel.com/960/400/" alt="Banner provicional"/>
+    <figcaption>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at ex facilis fuga, impedit quod?
+        </p>
+    </figcaption>
+    </figure>
+</section>
+
+<section id="contenido">
+    <article class="noticias">
+        <figure>
+            <img src="http://lorempixel.com/200/300" alt=""/>
+
+            <figcaption>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur.
+                </p>
+            </figcaption>
+        </figure>
+    </article>
+
+    <article class="agenda">
+        <div class="conciertos"></div>
+        <div class="cine"></div>
+    </article>
+</section>
+<aside id="aside">
+    <div class="aside_info">
+        <figure>
+            <img src="http://lorempixel.com/100/150" alt=""/>
+
+        </figure>
+    </div>
+
+    <div class="aside_info">
+        <figure>
+            <img src="http://lorempixel.com/100/150" alt=""/>
+        </figure>
+    </div>
+
+    <div class="aside_info">
+        <figure>
+            <img src="http://lorempixel.com/100/150" alt=""/>
+        </figure>
+    </div>
+</aside>
+
+<footer>
+    <p>
+        Desarrollado por Axel Lanuza
+    </p>
+    <p>
+        Derechos reservados &copy; 2015
+    </p>
+</footer>
 </body>
 </html>
